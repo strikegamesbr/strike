a348d643fd12aed3eec5908de4d73cf1ed51a1bd
Guilherme Athaides <guilherme.athaides@gmail.com>
2017-09-15 16:53:10 -0300

    PlayFab 1

---
+++ Raggabond Game Project/Assets/Scripts/Leaderboard/FacebookAndPlayFabInfo.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//por Leonardo Thurler
+//classe estática que que guarda os dados do usuário no jogo.
+
+public static class FacebookAndPlayFabInfo
+{
+	//Id do usuário no PlayFab
+	public static string userPlayFabId = "";
+	//Id do usuário no Facebook
+	public static string userFacebookId = "";
+	//Nome do usuário no Facebook
+	public static string userName = "";
+	//Indica se o usuário já está logado no PlayFab
+	public static bool isLoggedOnPlayFab = false;
+	//Indica se os dados do leaderboard já foram carregados.
+	public static bool leaderboardLoaded = false;
+	//Indica se os dados do leaderboard estão sendo carregados.
+	public static bool leaderboardIsLoading = false;
+}
